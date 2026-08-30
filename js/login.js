a66f1ffae76621d1c8744ef815f49b6115e36895
Luis Ismael Blanco Chambi <luis.blanco.c@ucb.edu.bo>
2026-08-29 21:13:38 -0400

    ajuste carpetas

--- js/login.js
+++ js/login.js
@@ -2,5 +2,5 @@ const formularioLogin = document.querySelector("#formulario-login");
 
 formularioLogin.addEventListener("submit", function (evento) {
     evento.preventDefault();
-    window.location.href = "dashboard.html";
+    window.location.href = "pages/dashboard.html";
 });
